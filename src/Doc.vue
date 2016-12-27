f09466fd9939cf56f44c0f24c0026bbb94b2f8ab
bigzhu <vermiliondun@gmail.com>
2016-12-27 11:32:28 +0800

    fox

---
+++ src/Doc.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <doc :name="name"
+      :desc="desc"
+      :parm_desc="parm_desc"
+      :parms="parms"
+      :code="code"
+      >
+    </doc>
+    <bz></bz>
+  </div>
+</template>
+<script>
+  // import toastr from 'toastr'
+
+  import Bz from './Bz'
+  import Doc from 'bz-doc'
+  export default {
+    components: {
+      Bz,
+      Doc
+    },
+    data: function () {
+      return {
+        loading: false,
+        name: 'bz-snow-fox',
+        desc: '狐狸',
+        parms: [
+          {parm: '', desc: ''}
+        ],
+        parm_desc: ``,
+        code: `<bz></bz>`
+      }
+    },
+    methods: {
+    }
+  }
+</script>
